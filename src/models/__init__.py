cab3786e59b81ac6e87f99d8968466e52b23bc0c
Pranjal Patel <pranjalpatel0903@gmail.com>
2025-10-01 00:09:45 +0530

    Update __init__.py
    Initialize models package with exports.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,1 +1,9 @@
+"""
+Models package for Hospital Appointment Scheduler
+"""
 
+from .patient import Patient
+from .doctor import Doctor
+from .appointment import Appointment, AppointmentStatus
+
+__all__ = ['Patient', 'Doctor', 'Appointment', 'AppointmentStatus']
